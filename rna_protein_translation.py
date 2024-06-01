60e0b4391a1cbe66676a38aed9ee93cc655cff38
khoryongteng <yongtengkhor338@gmail.com>
2024-06-01 20:55:25 +0100

    https://rosalind.info/problems/prot/

---
+++ rna_protein_translation.py
@@ -0,0 +1,43 @@
+# https://rosalind.info/problems/prot/
+
+import argparse
+import sys
+
+rna_codon_dict = {
+    # AUG -> Methionine always indicates the start of translation
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+}
+
+def translate(rna_string):
+    try:
+        protein_string = ""
+        for i in range(0, len(rna_string), 3):
+            codon = rna_string[i:i+3]
+            if rna_codon_dict[codon] == 'Stop':
+                return protein_string
+            protein_string += rna_codon_dict[codon]
+        raise Exception("No Stop Codon Found")
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('arg1', type=str, help='RNA String')
+    args = parser.parse_args()
+    print(translate(args.arg1))
